dd381bd9693de468c16e419a40e6e4db8813f0da
danielbmancini <danielbmancini@gmail.com>
2019-07-08 09:06:11 -0400

    totally generalized for 12º problem

--- Cap. 7/src/ex7_12.java
+++ Cap. 7/src/ex7_12.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
+//generalized solution (excludes the necessity to enter in the 10-100 interval) for the 12º problem in the 7º chapter.
 public class ex7_12 { /* Resolve uma versão adaptada do exercício 12 do capítulo 7, onde se desconsidera a necessidade de entradas no intervalo de 10 a 100.*/
     private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) { //problema convoluto..
-        int[] fivenum = new int[5];
+        int[] fivenum = new int[5]; //5 is arbitrary to the problem / 5 é arbitrário para o problema
 
         printAndFill(fivenum);
         printNonDuplicatesEntradas(fivenum);
@@ -13,12 +13,12 @@ public class ex7_12 { /* Resolve uma versão adaptada do exercício 12 do capít
 
     private static void printAndFill(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            System.out.printf("%n Enter number %d/5:", i + 1);
+            System.out.printf("%n Enter number %d/%d:", i + 1,array.length);
             array[i] = scan.nextInt();
         }
     }
 
-    private static boolean[] isnotUnique(int[] array, int index) {
+    private static boolean[] isNotUnique(int[] array, int index) {
         boolean[] duplicates = new boolean[array.length];
         Arrays.fill(duplicates, false); // desnecessário; apenas usado para fins de generalização
         for (int j = 0; j < array.length; j++)
@@ -31,7 +31,7 @@ public class ex7_12 { /* Resolve uma versão adaptada do exercício 12 do capít
 
     private static void printNonDuplicatesEntradas(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            if (!isnotUnique(array, i)[i]) {
+            if (!isNotUnique(array, i)[i]) {
                 System.out.println(array[i]);
             }
         }
