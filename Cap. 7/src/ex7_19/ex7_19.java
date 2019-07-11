d9a4e4e361e448bf1a6ccd9c3d6f7530ca3a053c
danielbmancini <danielbmancini@gmail.com>
2019-07-11 07:30:33 -0400

    Updates

--- Cap. 7/src/ex7_19/ex7_19.java
+++ Cap. 7/src/ex7_19/ex7_19.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 class ex7_19 {
 
     private static Scanner scan = new Scanner(System.in);
+    private static int classIsFullUsage = 0;
 
     static void exitIfFull(boolean[] assentos) { //exits when the plane is full
         if (assentos[(assentos.length / 2) - 1] && assentos[assentos.length - 1]) //if the last seat of both classes are occupied
@@ -14,27 +15,28 @@ class ex7_19 {
         }
     }
 
-    static void afterChoice(int choice,boolean[] assentos) {
+    static void afterChoice(int choice, boolean[] assentos) {
         for (int i = 0; i < assentos.length; i++) {
             if (choice == 1 && i < assentos.length / 2 && !assentos[i]) //se a primeira classe é a referida por choice
             {
-                System.out.printf("%nHave a good flight - your seat is n. %d", i + 1);
+                System.out.printf("%nHave a good flight - your seat is n. %d%n", i + 1);
                 assentos[i] = true;
                 break;
-            }
-            else if (choice == 2 && i >= assentos.length / 2 && !assentos[i]) {
-                System.out.printf("%nHave a good flight - your seat is n. %d", i + 1);
+            } else if (choice == 2 && i >= assentos.length / 2 && !assentos[i]) {
+                System.out.printf("%nHave a good flight - your seat is n. %d%n", i + 1);
                 assentos[i] = true;
                 break;
             }
         }
     }
 
-    static void classIsFull(int choice,boolean[] transfer) {
+    static void classIsFull(int choice, boolean[] transfer) {
         String choice2;
         exitIfFull(transfer);
 
-        if ((choice == 1 && transfer[(transfer.length/2) - 1]) || (choice == 2 && transfer[transfer.length - 1])) { //haja uso de condições
+
+        if ((choice == 1 && transfer[(transfer.length / 2) - 1] && classIsFullUsage != 1) ||
+                (choice == 2 && transfer[transfer.length - 1] && classIsFullUsage != 1)) { //haja uso de condições
             System.out.println("\nthe chosen class is full. do you wish to be in the other one, instead? (y/n)"); //i won't tweak to make it say the converse flight class
             choice2 = scan.nextLine();
             if (choice2.equals("y")) {
@@ -46,10 +48,11 @@ class ex7_19 {
                         choice = 1;
                         break;
                 }
-                afterChoice(choice,transfer);
+                afterChoice(choice, transfer);
             } else if (choice2.equals("n"))
                 System.out.println("\nNext flight departs in 3 hours.");
-
         }
+        if (transfer[(transfer.length / 2) - 2] || transfer[transfer.length-2]) //indirectly prevents classIsFull from being called when the last seat of each class is being occupied
+            classIsFullUsage++;
     }
 }
--- Cap. 7/src/ex7_19/ex7_19_run.java
+++ Cap. 7/src/ex7_19/ex7_19_run.java
@@ -1,23 +1,25 @@
 package ex7_19;
-import java.util.Scanner;
-import java.util.Arrays;
 
-public class ex7_19_run{
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ex7_19_run {
     private static boolean[] assentos = new boolean[10]; //10 is arbitrary, the app can use any even value (which the number of first-class seats are the same of the economic-class)
     private static int choice;
+
     static {
         Arrays.fill(assentos, false);
     }
 
-    public static void main(String[] args) { //main ends when the ex7_19.exitIfFull if condition is true
+    public static void main(String[] args) { //main 'ends' when the ex7_19.exitIfFull if condition is true
         Scanner scan = new Scanner(System.in);
+        ex7_19.exitIfFull(assentos);
+        ex7_19.classIsFull(choice, assentos); //checks if class is full, to switch if so
 
         System.out.printf("%nInsert 1 for first-class, or 2 for economic.");
         choice = scan.nextInt();
 
-        ex7_19.exitIfFull(assentos);
-        ex7_19.afterChoice(choice,assentos);
-        ex7_19.classIsFull(choice,assentos); //checks if class is full, to switch if so2
+        ex7_19.afterChoice(choice, assentos);
         main(args);
 
     }
