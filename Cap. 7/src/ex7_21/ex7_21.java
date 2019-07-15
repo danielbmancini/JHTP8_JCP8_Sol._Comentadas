9b15d5677a5f723283af0f04b470362ca86e8d86
danielbmancini <danielbmancini@gmail.com>
2019-07-15 08:17:44 -0400

    Updates

---
+++ Cap. 7/src/ex7_21/ex7_21.java
@@ -0,0 +1,127 @@
+package ex7_21;
+
+import java.util.Arrays;
+
+class ex7_21 {
+    private static boolean pen_state;
+    private static int[][] floor = new int[20][20];
+    private static int[] current_block = {0, 0}; //should be valued at most (19,19)
+    private static Integer direction = 1; // from a same frame of reference, 0: up, 1: right, 2: down, 3: left
+
+    static {
+        for (int i = 0; i < floor.length; i++)
+            Arrays.fill(floor[i], 0);
+    }
+
+    private static void fillFloor(int i, int j) {
+        switch (direction) {
+            case 0:
+                while (i <= j) {
+                    floor[j][current_block[1]] = 1;
+                    j--;
+                }
+                break;
+            case 1:
+                while (i >= j) {
+                    floor[current_block[0]][j] = 1;
+                    j++;
+                }
+                break;
+            case 2:
+                while (i >= j) {
+                    floor[j][current_block[1]] = 1;
+                    j++;
+                }
+                break;
+            case 3:
+                while (j >= i) {
+                    floor[current_block[0]][j] = 1;
+                    j--;
+                }
+        }
+    }
+
+    static void one() {
+        pen_state = false;
+    }
+
+    static void two() {
+        pen_state = true;
+    }
+
+    static void three()//right
+    {
+        if (direction == 3)
+            direction = 0;
+        else {
+            if (direction == 0)
+                direction = 1;
+            else if (direction < 3)
+                direction++;
+        }
+    }
+
+    static void four()//left
+    {
+        if (direction == 0)
+            direction = 3;
+        else if (direction > 0)
+            direction--;
+    }
+
+    static void five(int distance) //convoluted
+    {
+        switch (direction) {
+            case 0:
+                if (current_block[0] - distance >= 0) {
+                    int storeCurrentBlock = current_block[0];
+                    current_block[0] += -distance;
+                    if (pen_state)
+                        fillFloor(current_block[0], storeCurrentBlock);
+                } else System.out.println("Can't go up!");
+                break;
+            case 1:
+                if (current_block[1] + distance <= 19) {
+                    int storeCurrentBlock1 = current_block[1];
+                    current_block[1] += distance;
+                    if (pen_state)
+                        fillFloor(current_block[1], storeCurrentBlock1);
+                }
+                else if (distance >= 20)
+                    if (!(current_block[1] + distance <= 18))
+                        System.out.println("Can't go to the right!");
+                break;
+            case 2:
+                if (current_block[0] + distance <= 19) {
+                    int storeCurrentBlock = current_block[0];
+                    current_block[0] += distance;
+                    if (pen_state)
+                        fillFloor(current_block[0], storeCurrentBlock);
+                } else System.out.println("Can't go down!");
+                break;
+            case 3:
+                if (current_block[1] - distance >= 0) {
+                    int storeCurrentBlock1 = current_block[1];
+                    current_block[1] += -distance;
+                    if (pen_state)
+                        fillFloor(current_block[1], storeCurrentBlock1);
+                }
+                else if (current_block[0] == 0)
+                    System.out.println("Can't go to the left!");
+                break;
+        }
+    }
+
+    static void six() {
+        for (int i = 0; i < floor.length; i++)
+            for (int j = 0; j < floor[i].length; j++) {
+                System.out.print(floor[i][j] + "  ");
+                if (j == floor[i].length - 1)
+                    System.out.println("\n");
+            }
+    }
+
+    static void nine() {
+        System.exit(0);
+    }
+}
---
+++ Cap. 7/src/ex7_21/ex7_21_run.java
@@ -0,0 +1,50 @@
+package ex7_21;
+
+import java.util.Scanner;
+
+public class ex7_21_run {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Insert a command:");
+        int command = scan.nextInt();
+        switch (command) {
+            case 1:
+                ex7_21.one();
+                main(args);
+                break;
+            case 2:
+                ex7_21.two();
+                main(args);
+                break;
+            case 3:
+                ex7_21.three();
+                main(args);
+                break;
+            case 4:
+                ex7_21.four();
+                main(args);
+                break;
+            case 5: //starts pointing to the right, can go up to 20 blocks
+                System.out.println("Input a non negative distance for command '5':");
+                int dist = scan.nextInt();
+                if (dist >= 0 && dist != 1)
+                    ex7_21.five(dist - 1);
+                else if (dist != 1) {
+                    System.out.println("Can't input negative distance.");
+                    main(args);
+                    break;
+                }
+                if (dist == 1)
+                    ex7_21.five(1);
+                main(args);
+                break;
+            case 6:
+                ex7_21.six();
+                main(args);
+                break;
+            case 9:
+                ex7_21.nine();
+                break;
+        }
+    }
+}
