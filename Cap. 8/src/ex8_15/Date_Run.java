75aa8094358ef2c10d85403bae87ef57cb0e8551
daniel <danielbmancini@gmail.com>
2019-08-02 09:45:46 -0400

    Updates

---
+++ Cap. 8/src/ex8_15/Date_Run.java
@@ -0,0 +1,11 @@
+package ex8_15;
+
+public class Date_Run {
+    public static void main(String[] args) {
+        Date aDate = new Date(month.September, 11, 2019);
+
+        System.out.println(aDate.toString_a());
+        System.out.println(aDate.toString_b());
+        System.out.println(aDate.toString_c());
+    }
+}
---
+++ Cap. 8/src/ex8_15/month.java
@@ -0,0 +1,24 @@
+package ex8_15;
+
+enum month {
+    January(1, 1), February(2, 32), March(3, 60), April(4, 91),
+    May(5, 121), June(6, 152), July(7, 182), August(8, 213),
+    September(9, 244), October(10, 274), November(11, 305),
+    December(12, 335);
+
+    private int monthNumber;
+    private int daysAfterNewYear;
+
+    month(int monthNumber, int daysAfterNewYear) {
+        this.monthNumber = monthNumber;
+        this.daysAfterNewYear = daysAfterNewYear;
+    }
+
+    int getMonthNumber() {
+        return monthNumber;
+    }
+
+    int getDaysAfterNewYear() {
+        return daysAfterNewYear;
+    }
+}
