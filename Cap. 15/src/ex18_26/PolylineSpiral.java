26f59e980bfe9ca36e4ce232f92efa8e46f5199d
daniel <danielbmancini@gmail.com>
2019-11-17 09:29:39 -0400

    Updates

---
+++ Cap. 15/src/ex18_26/PolylineSpiral.java
@@ -0,0 +1,16 @@
+package ex18_26;
+
+import javax.swing.JFrame;
+
+public class PolylineSpiral {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+
+        PolylineSpiralJPanel polylineSpiralJPanel = new PolylineSpiralJPanel();
+
+        jFrame.add(polylineSpiralJPanel);
+        jFrame.setSize(1000, 1000);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
