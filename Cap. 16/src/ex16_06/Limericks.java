addd96f073321e8b0847093f5437889f5f65bfa1
daniel <danielbmancini@gmail.com>
2019-12-23 00:04:14 -0400

    Updates

--- Cap. 16/src/ex16_06/Limericks.java
+++ Cap. 16/src/ex16_06/Limericks.java
@@ -1,10 +1,11 @@
 /*
- * So, i guess a more efficient way to this kind of operation is by using ML to vectorize different limericks, in terms
+ * A more efficient way to this kind of operation would be by using ML to vectorize different limericks, in terms
  * of their word class structure, as prescribed by the book. Then, store the date outside the class and use java.io libraries.
  * That misses the scope of the chapter this exercise is situated, so i'm not doing it - everything will be manual.
  */
 package ex16_06;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Limericks {
@@ -41,87 +42,52 @@ public class Limericks {
     //3 different limerick structures vectors referencing getReferences()' references.
     //each sub array corresponds to one verse.
     //this is a structuralist approach, you could say.
-    private int[][] limerickStructure_1 = {{1, 10, 2, 0, 1, 11, 10},
+    private static int[][] limerickStructure_1 = {{1, 10, 2, 0, 1, 11, 10},
             {9, 3, 1, 11, 10},
             {8, 2, 11, 0, 10},
             {11, 1, 10, 0, 8, 10},
             {8, 7, 10, 0, 1, 10}};
 
-    private int[][] limerickStructure_2 = {{8, 7, 2, 1, 10, 0, 10},
+    private static int[][] limerickStructure_2 = {{8, 7, 2, 1, 10, 0, 10},
             {9, 3, 11, 6, 2, 0, 10},
             {8, 2, 1, 10},
             {7, 2, 7, 10},
             {7, 1, 10, 2, 8, 9, 1, 10}};
 
-    private int[][] limerickStructure_3 = {{8, 4, 1, 11, 10, 2, 10},
+    private static int[][] limerickStructure_3 = {{8, 4, 1, 11, 10, 2, 10},
             {7, 11, 8, 2, 10},
             {8, 3, 2, 0, 1, 10},
             {5, 11, 0, 1, 10},
             {9, 7, 2, 7, 0, 10}};
 
+    private static ArrayList<int[][]> structuresList = new ArrayList<>(3);
+
+    static {
+        structuresList.add(limerickStructure_1);
+        structuresList.add(limerickStructure_2);
+        structuresList.add(limerickStructure_3);
+    }
+
     public String generateLimerick() {
         StringBuilder limerick = new StringBuilder();
         limerick.append("\n");
-        int limerickStructure = random.nextInt(3) + 1;
+        int structIndex = random.nextInt(3);
         int counter;
         int k1 = random.nextInt(4); //which array of rhyming nouns to use 1
         int k2 = random.nextInt(4); //which array of rhyming nouns to use 2
-        System.out.printf("%n%nUsing structure %d.", limerickStructure);
-        switch (limerickStructure) {
-            case 1:
-                for (int i = 0; i < 5; i++) { //loops through the 5 arrays in limerickStructure_1
-                    counter = 0;
-                    limerick.append("\n");
-                    for (int k : limerickStructure_1[i]) {
-                        if (counter != limerickStructure_1[i].length - 1) {
-                            String[] foo = getReference(k);
-                            if (counter == 0) {
-                                int index = random.nextInt(foo.length);
-                                limerick.append(Character.toUpperCase(foo[index].charAt(0))).append(foo[index].substring(1)).append(" ");
-                            } else
-                                limerick.append(foo[random.nextInt(foo.length)]).append(" ");
-                        } else if (i == 0 || i == 1 || i == 4) //these two here and in the other cases refer to the rhyming schemes of limericks (AABBA)
-                            limerick.append(nouns[k1][random.nextInt(nouns[k1].length)]);
-                        else
-                            limerick.append(nouns[k2][random.nextInt(nouns[k2].length)]);
-                        counter++;
-                    }
-                }
-
-                return limerick.toString();
-            case 2:
-                for (int i = 0; i < 5; i++) { //loops through the 5 arrays in limerickStructure_2
-                    counter = 0;
-                    limerick.append("\n");
-                    for (int k : limerickStructure_2[i]) {
-                        if (counter != limerickStructure_2[i].length - 1) {
-                            String[] foo = getReference(k);
-                            if (counter == 0) {
-                                int index = random.nextInt(foo.length);
-                                limerick.append(Character.toUpperCase(foo[index].charAt(0))).append(foo[index].substring(1)).append(" ");
-                            } else
-                                limerick.append(foo[random.nextInt(foo.length)]).append(" ");
-                        } else if (i == 0 || i == 1 || i == 4)
-                            limerick.append(nouns[k1][random.nextInt(nouns[k1].length)]);
-                        else
-                            limerick.append(nouns[k2][random.nextInt(nouns[k2].length)]);
-                        counter++;
-                    }
-                }
-                return limerick.toString();
-        }
-        for (int i = 0; i < 5; i++) { //loops through the 5 arrays in limerickStructure_3
-            limerick.append("\n");
+        System.out.printf("%n%nUsing structure %d.", structIndex + 1);
+        for (int i = 0; i < 5; i++) { //loops through the 5 arrays an limerick structure
             counter = 0;
-            for (int k : limerickStructure_3[i]) {
-                if (counter != limerickStructure_3[i].length - 1) {
+            limerick.append("\n");
+            for (int k : structuresList.get(structIndex)[i]) {
+                if (counter != structuresList.get(structIndex)[i].length - 1) {
                     String[] foo = getReference(k);
                     if (counter == 0) {
                         int index = random.nextInt(foo.length);
                         limerick.append(Character.toUpperCase(foo[index].charAt(0))).append(foo[index].substring(1)).append(" ");
                     } else
                         limerick.append(foo[random.nextInt(foo.length)]).append(" ");
-                } else if (i == 0 || i == 1 || i == 4)
+                } else if (i == 0 || i == 1 || i == 4) //to order the rhyming scheme of limericks (AABBA)
                     limerick.append(nouns[k1][random.nextInt(nouns[k1].length)]);
                 else
                     limerick.append(nouns[k2][random.nextInt(nouns[k2].length)]);
